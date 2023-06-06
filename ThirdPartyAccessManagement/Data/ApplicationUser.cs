ab599aa8907eebb1cdfd7d5639adc66a9b390e6b
Biraj <birajkmc4@gmail.com>
2023-06-06 11:43:03 +0545

    Add project files.

---
+++ ThirdPartyAccessManagement/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ThirdPartyAccessManagement.Data
+{
+    public class UserManager : IdentityUser
+    {
+        public string? Name { get; set; }
+    }
+}
